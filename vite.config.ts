42a19b95b3e14712bc43c380a22b5c8db44e7b50
jimmyphangit <jimmyphan1@hotmail.com>
2025-08-19 19:45:57 -0400

    Removed base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/personal-budgeting-web-app/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
